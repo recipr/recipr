1f091557212a51bf4fdc337e27244bc39a560aa3
Slemgrim <slemgrim@gmail.com>
2014-06-11 22:47:42 +0200

    connect toggle to tab

--- recipr/web/res/scripts/main.dart
+++ recipr/web/res/scripts/main.dart
@@ -1,9 +1,20 @@
 import 'package:recipr/recipr.dart';
 import 'package:polymer/polymer.dart';
+import 'package:recipr/components/toggle/main.dart';
+import 'package:recipr/components/tabs/main.dart';
+
+import 'dart:html';
 
 Recipr recipr;
 
 void main() {
     initPolymer();
+
+    ReciprTabs tab = querySelector('recipr-tabs');
+    ReciprToggle toggle = querySelector('recipr-toggle');
+
+    toggle.onToggle.listen((String value){
+      tab.setTab(value);
+    });
 }
 
