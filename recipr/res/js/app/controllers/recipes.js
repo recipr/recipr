7b58e6c803e271d67fc7eaaaeb0bb080eb784990
Slemgrim <slemgrim@gmail.com>
2014-06-21 21:23:12 +0200

    partials for tabs

--- recipr/res/js/app/controllers/recipes.js
+++ recipr/res/js/app/controllers/recipes.js
@@ -1,5 +1,9 @@
-App.RecipesController = Ember.Controller.extend({
-  //TabState
+App.RecipesController = Ember.ObjectController.extend({
   showIngredients: true,
-  showPreparation: false
+  showPreparation: false,
+  
+  hasMultipleSections: function() {
+    var sections = this.get('model.sections');
+    return sections.get('length') > 1 ? true : false;
+  }.property('model.@each.sections')
 });
