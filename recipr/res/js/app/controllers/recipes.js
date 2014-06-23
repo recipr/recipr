b16576d5f426764def499fb3829a7ae9f30a4ceb
Slemgrim <slemgrim@gmail.com>
2014-06-23 22:39:18 +0200

    remove console logs

--- recipr/res/js/app/controllers/recipes.js
+++ recipr/res/js/app/controllers/recipes.js
@@ -3,15 +3,12 @@ App.RecipesController = Ember.ObjectController.extend({
   showPreparation: false,
 
   hasMultipleSections: function() {
-    console.log('section test');
     var sections = this.get('model.sections');
     return sections.get('length') > 1 ? true : false;
   }.property('model.sections.length'),
 
   actions: {
     addSection: function(){
-      console.log('addSection');
-
       var section = this.store.createRecord('Section');
       var sections = this.get('model.sections');
       sections.pushObject(section);
