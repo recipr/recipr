dde32abd6b28c496e60c0b9cfa201dc44e5b23e1
Slemgrim <slemgrim@gmail.com>
2014-06-24 22:39:58 +0200

    refactor ingredient handling

--- recipr/res/js/app/components/ingredient.js
+++ recipr/res/js/app/components/ingredient.js
@@ -1,47 +1,15 @@
 App.ReciprIngredientComponent = Ember.Component.extend({
     classNames: ['recipr-ingredient'],
 
-    sketch: '',
     quantity: '',
     unit: '',
     name: '',
 
     ingredient: null,
-    section: null,
-
-    isSketch: function(){
-      var length = this.get('name.length');
-      return length < 1;
-    }.property('name'),
-
-    regex: /^([\d.,\/]*)(\s*([^\W]*)\s+(.*))?/i,
 
     actions: {
-      add: function(){
-        this.parseSketch();
-        if(this.get('name.length') > 0){
-          this.sendAction('add', {
-            'section': this.get('section'),
-            'quantity': this.get('quantity'),
-            'unit': this.get('unit'),
-            'name': this.get('name')
-          });
-        }
-      },
       remove: function(){
         this.sendAction('remove', this.get('ingredient'));
       },
-    },
-
-    parseSketch: function(){
-        this.set('sketch', this.filterWhitespaces(this.get('sketch')));
-        var match = this.regex.exec(this.get('sketch'));
-        this.set('quantity', match[1] ? match[1] : "1");
-        this.set('unit', match[3] ? match[3] : "");
-        this.set('name', match[4] ? match[4] : "");
-    },
-
-    filterWhitespaces: function(text){
-        return text.replace(/\s{2,}/g, ' ').trim();
     }
 });
--- recipr/res/js/app/controllers/recipes.js
+++ recipr/res/js/app/controllers/recipes.js
@@ -2,6 +2,10 @@ App.RecipesController = Ember.ObjectController.extend({
   showIngredients: true,
   showPreparation: false,
 
+  newIngredient: '',
+
+  regex: /^([\d.,\/]*)(\s*([^\W]*)\s+(.*))?/i,
+
   hasMultipleSections: function() {
     var sections = this.get('model.sections');
     return sections.get('length') > 1 ? true : false;
@@ -18,13 +22,26 @@ App.RecipesController = Ember.ObjectController.extend({
         ingredient.deleteRecord();
     },
 
-    addIngredient: function(data){
-        var ingredient = this.store.createRecord('ingredient', {
-          quantity: data.quantity,
-          unit: data.unit,
-          name: data.name
-        });
-        data.section.get('ingredients').pushObject(ingredient);
+    addIngredient: function(section){
+        var ingredient = this.createIngredientFromName();
+        section.get('ingredients').pushObject(ingredient);
     },
+  },
+
+  createIngredientFromName: function(name){
+      name = this.filterWhitespaces(this.get('newIngredient'));
+      var match = this.regex.exec(name);
+
+      var ingredient =this.store.createRecord('ingredient', {
+        quantity: match[1] ? match[1] : "1",
+        unit: match[3] ? match[3] : "",
+        name: match[4] ? match[4] : ""
+      });
+      this.set('newIngredient', '');
+      return ingredient;
+  },
+
+  filterWhitespaces: function(text){
+      return text.replace(/\s{2,}/g, ' ').trim();
   }
 });
