3b7e5a5d5fb8ff8df6386394a1b70970d204a55b
Slemgrim <slemgrim@gmail.com>
2014-08-08 16:53:46 +0200

    fix routing error

--- recipr-app/app/router.js
+++ recipr-app/app/router.js
@@ -8,12 +8,12 @@ Router.map(function() {
     this.route('index', { path: '/' });
     this.route('dashboard');
 
-    this.route('recipes');
-
-    this.resource('recipe', { path: 'recipe/:id'}, function() {
+    this.resource('recipe', { path: '/recipe/:recipe_id' }, function(){
         this.route('preparation');
         this.route('ingredients');
-    });
+    }) 
+
+    this.resource('recipes');
 
     this.resource('settings', function() {
         this.route('user');
