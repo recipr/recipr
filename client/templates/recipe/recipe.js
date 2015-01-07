28a9deb452ecb88ad510000e0144846ca37e23ac
Slemgrim <slemgrim@gmail.com>
2015-01-07 22:03:56 +0100

    cleanup routes

--- client/templates/recipe/recipe.js
+++ client/templates/recipe/recipe.js
@@ -10,23 +10,15 @@ Template.recipe.events({
 
     var ingredients = Session.get('ingredients');
 
-		if(this.isNew){
-			var data = {
-				title: title,
+		var recipeId = this._id ? this._id : null;
+
+    var data = {
+        title: title,
         intro: intro,
         ingredients: ingredients
-			};
-      Meteor.call("saveRecipe", data);
-		} else {
-			var recipe = this.recipe();
-      var data = {
-          title: title,
-          intro: intro,
-          ingredients: ingredients
-      };
-      Meteor.call("saveRecipe", data, recipe._id);
-		}
-
+    };
+    Meteor.call("saveRecipe", data, recipeId);
+		
     if(ingredients.length){
       Meteor.call('saveIngredients', ingredients);
     }
@@ -74,8 +66,8 @@ Template.recipe.helpers({
 })
 
 Template.recipe.created = function(){
-  if(this.data.recipe){
-    Session.set('ingredients', this.data.recipe().ingredients);
+  if(this.data && this.data.ingredients){
+    Session.set('ingredients', this.data.ingredients);
   } else {
     Session.set('ingredients', []);
   }
