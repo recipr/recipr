1b0788032143dfac811c72d23ea293a706bf740e
Slemgrim <slemgrim@gmail.com>
2015-01-26 21:52:22 +0100

    more styling

--- client/templates/layout/application/application.js
+++ client/templates/layout/application/application.js
@@ -1,3 +1,22 @@
+Template.ApplicationLayout.rendered = function(){
+  this.onScroll = function(event){
+    var oldScrollPos = Session.get('scroll-pos');
+    var newScrollPos = window.scrollY;
+
+    if(oldScrollPos > newScrollPos){
+      Session.set('scroll-dir', -1);
+    } else if(oldScrollPos == newScrollPos) {
+      Session.set('scroll-dir', 0);
+    } else {
+      Session.set('scroll-dir', 1);
+    } 
+
+    Session.set('scroll-pos', newScrollPos);
+  }
+
+  window.addEventListener('scroll', this.onScroll, false);
+}
+
 Template.ApplicationLayout.events({
   "click .menu-toggle": function (event) {
     Session.set('showSidebar', !Session.get('showSidebar'));
@@ -10,5 +29,15 @@ Template.ApplicationLayout.events({
 
   "mouseout [data-tooltip]": function(event){
     Session.set('showTooltip', false);
+  },
+
+  "scroll .app-header": function(event){
+    console.log('scroll');
+  }
+});
+
+Template.ApplicationLayout.helpers({
+  hideHeader: function(){
+    return Session.get('scroll-dir') == 1;
   }
 });
