22f763c63e15d2ee454c17b33549c3c41698c1a4
Slemgrim <slemgrim@gmail.com>
2015-01-30 21:25:32 +0100

    fake placeholder for editor

--- client/templates/gui/editor/editor.js
+++ client/templates/gui/editor/editor.js
@@ -1,4 +1,10 @@
+Template.editor.created = function(){
+  this.length = new Blaze.ReactiveVar();
+  this.length.set(0);
+}
+
 Template.editor.rendered = function(){
+  var self = this;
 
   var template = Template.instance();
   var editor = template.find('.content');
@@ -6,10 +12,16 @@ Template.editor.rendered = function(){
 
   this.quill = new Quill(editor);
   this.quill.addModule('toolbar', { container: toolbar });
+  self.length.set(self.quill.getLength());
+
+
+  this.quill.on('text-change', function() {
+    self.length.set(self.quill.getLength());
+  });
 }
 
-Template.editor.events({
-  "keyup .content": function(event, template){
-    console.log(template.quill.getContents());
+Template.editor.helpers({
+  empty: function(){
+    return Template.instance().length.get() <= 1;
   }
 });
