2159126dd9a3bd2f18a54b4936a7de2722d48fbe
Slemgrim <slemgrim@gmail.com>
2015-03-04 21:50:48 +0100

    more editor styles

--- client/templates/gui/editor/editor.js
+++ client/templates/gui/editor/editor.js
@@ -1,6 +1,8 @@
 Template.editor.created = function(){
   this.length = new Blaze.ReactiveVar();
   this.length.set(0);
+  this.visible = new Blaze.ReactiveVar();
+  this.visible.set(false);
 }
 
 Template.editor.rendered = function(){
@@ -10,11 +12,18 @@ Template.editor.rendered = function(){
   var editor = template.find('.content');
   var toolbar = template.find('.toolbar')
 
-  this.quill = new Quill(editor);
+  this.quill = new Quill(editor, {
+    theme: 'snow'
+  });
   this.quill.addModule('toolbar', { container: toolbar });
+
+  this.quill.on('selection-change', function(range) {
+    if (range !== null) {
+      self.visible.set(true);
+    }
+  });
+
   self.length.set(self.quill.getLength());
-
-
   this.quill.on('text-change', function() {
     self.length.set(self.quill.getLength());
   });
@@ -23,5 +32,8 @@ Template.editor.rendered = function(){
 Template.editor.helpers({
   empty: function(){
     return Template.instance().length.get() <= 1;
+  },
+  isVisible: function(){
+    return Template.instance().visible.get();
   }
 });
